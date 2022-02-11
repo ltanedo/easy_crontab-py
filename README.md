4759bc21fa11104710e6e1e516efc77db1fe3953
Lloyd Tanedo <hackintosh@Lloyds-MacBook-Pro.local>
2022-02-11 12:04:31 -0500

    added pypi config

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# easy-crontab
+# Easy Environment
+> only works with bash and zsh on unix (linux and mac).  
+
+A python library to rapidly set, get, and view local environment variables (currently only for bash and zsh shells in unix).
+
+[ [PyPi Link](https://pypi.org/project/easy-environment/0.1/) ]
+
+
+### Example Usage Below
+
+```
+import easy_environment as environ
+
+
+""" 
+get env dict
+================
+ - input  : None
+ - output : list[dict]
+"""
+env_dict = environ.get_env_dict()
+
+
+""" 
+get env var
+================
+ - input  : (str) key
+ - output : (str) value
+"""
+env_dict = environ.get_env("EXAMPLE_KEY")
+
+
+""" 
+set env var
+================
+ - input  : (str) key, 
+            (str) value
+ - output : list[dict]
+"""
+# set env var -> None
+env_dict = environ.set_env_var("EXAMPLE_KEY", "EXAMPLE_VALUE)
+```
---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'easy_cron',         # How you named your package folder (MyLib)
+  packages = ['easy_cron'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'A library to set cron jobs from python',   # Give a short description about your library
+  author = 'lloydtan',                   # Type in your name
+  author_email = 'lloydtan@buffalo.edu',      # Type in your E-Mail
+  url = 'https://github.com/ltanedo/easy-crontab.git',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/ltanedo/easy-crontab/archive/refs/tags/v_01.tar.gz',    # I explain this later on
+  keywords = ['python', 'env', 'environment', 'cron', 'crontab', 'cronjob','job','schedule','scheduling'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
